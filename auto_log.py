c66f0870bb9735f2c6cc4374959869e332a6c092
Max Curran <curranmax@gmail.com>
2017-02-27 13:54:42 -0500

    first commit

---
+++ auto_log.py
@@ -0,0 +1,58 @@
+
+from power_meter import PowerMeter
+import time
+from math import log
+import argparse
+
+def convertWattsToDbm(v):
+	return 30.0 + 10 * log(v, 10)
+
+# test_length - float greater than 0 - length of test in SECONDS
+# TODO allow for a ctrl+c interrupt or something
+def autoLog(test_length, out_file):
+	num_iters = 0
+
+	power_meter = PowerMeter()
+	power_meter.setAverageCount(1)
+
+	if out_file != '':
+		f = open(out_file, 'w')
+		f.write('time(s) power(dBm)\n')
+	else:
+		f = None
+
+	start_time = time.time()
+	while True:
+		power = power_meter.getPower()
+		num_iters += 1
+
+		end_time = time.time()
+		delta_time = end_time - start_time
+		
+		if f != None:
+			f.write(str(delta_time) + ' ' + str(convertWattsToDbm(power)) + '\n')
+		else:
+			print convertWattsToDbm(power)
+		
+		if delta_time > test_length:
+			break
+	
+	if f != None:
+		f.close()
+
+	print 'Average time per log:', (end_time - start_time) * 1000.0 / float(num_iters), 'ms'
+	print 'Number of logs:', num_iters
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description = 'Constantly reads from power meter, and writes data to a file')
+
+	parser.add_argument('-tl', '--test_length', metavar = 'TEST_LENGTH', type = float, nargs = 1, default = [1.0], help = 'Length of the test in seconds')
+	parser.add_argument('-f', '--out_file', metavar = 'OUT_FILE_NAME', type = str, nargs = 1, default = [''], help = 'File to write output to. If no file given, then power is printed to stdout')
+	
+	args = parser.parse_args()
+
+	test_length = args.test_length[0]
+	out_file = args.out_file[0]
+
+	autoLog(test_length, out_file)
+
---
+++ power_meter.py
@@ -0,0 +1,45 @@
+
+# Requires PyVisa, PyVisa-py, and ThorlabsPM100
+# PyVisa : https://pyvisa.readthedocs.io/en/stable/
+# PyVisa-py : https://pyvisa-py.readthedocs.io/en/latest/
+# ThorlabsPM100 : https://pypi.python.org/pypi/ThorlabsPM100
+
+import visa
+from ThorlabsPM100 import ThorlabsPM100
+
+class PowerMeter:
+	def __init__(self, resource_id = ''):
+		if resource_id == '':
+			# Current default resource_id for our power meter
+			resource_id = 'USB0::4883::32888::P0015193::0::INSTR'
+		self.resource_id = resource_id
+		self.power_meter = None
+
+		self.open()
+
+	def open(self):
+		# '@py' specifies to use Pyvisa-py as the backend instead of NI VISA (which I can't installed on ubuntu)
+		rm = visa.ResourceManager('@py')
+		try:
+			inst = rm.open_resource(self.resource_id, timeout = 1)
+		except ValueError:
+			raise Exception('Must have root access! Try using sudo!')
+
+		# The ThorlabsPM100 requires \n for both types of termination. PyVisa defaults one of those to '\r'
+		inst.read_termination = '\n'
+		inst.write_termination = '\n'
+
+		self.power_meter = ThorlabsPM100(inst = inst)
+
+	def getPower(self):
+		if self.power_meter != None:
+			return self.power_meter.read
+		else:
+			raise Exception('Can\'t getPower, power_meter not opened')
+
+	def setAverageCount(self, v):
+		if self.power_meter != None:
+			# This value defaults to 1. Setting it to larger values will make each log take longer.
+			self.power_meter.sense.average.count = v
+		else:
+			raise Exception('Can\'t setAverageCount, power_meter not opened')
